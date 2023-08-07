44bd8157a79ee48d471fa6e98eea080837dca35f
brian tarqui <tarquibrian@gmail.com>
2023-08-07 00:18:38 -0400

    wrapper main config⚙

--- src/components/wrapperView.tsx
+++ src/components/wrapperView.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Aside from "./aside";
 
 const WrapperView = ({ children }: { children: React.ReactNode }) => {
-  return <div id="wrapperview">{children}</div>;
+  return (
+    <div id="wrapperview">
+      <div className="wrapperview__picture">
+        <div className="picture__content">l</div>
+      </div>
+      <div className="wrapperview__content">
+        <div className="wrapperview__content-aside">
+          <div className="aside__content">aside</div>
+        </div>
+        <div className="wrapperview__content-main">
+          <nav>nav</nav>
+          <main>{children}</main>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WrapperView;
