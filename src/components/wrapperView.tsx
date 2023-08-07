7e9ce45423038e9ab1bee838bc5dcc389c2b9448
brian tarqui <tarquibrian@gmail.com>
2023-08-07 00:54:17 -0400

    test

--- src/components/wrapperView.tsx
+++ src/components/wrapperView.tsx
@@ -9,7 +9,27 @@ const WrapperView = ({ children }: { children: React.ReactNode }) => {
       </div>
       <div className="wrapperview__content">
         <div className="wrapperview__content-aside">
-          <div className="aside__content">aside</div>
+          <div className="aside__content">
+            <div className="aside__content-picture">
+              <div className="picture__content">p</div>
+            </div>
+            <div className="aside__content-header">
+              <header>
+                <h1>Brian Tarqui Rojas</h1>
+                <h2>Full Stack Developer</h2>{" "}
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+                  unde aspernatur, praesentium velit ea reiciendis suscipit
+                </p>
+              </header>
+
+              <div className="socials">
+                <div className="">a</div>
+                <div className="">a</div>
+                <div className="">a</div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="wrapperview__content-main">
           <nav>nav</nav>
