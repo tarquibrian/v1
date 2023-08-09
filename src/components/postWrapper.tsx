a5de41e1e908dcef132a45cb98692681fa05782e
brian tarqui <tarquibrian@gmail.com>
2023-08-09 17:26:14 -0400

    test

--- src/components/postWrapper.tsx
+++ src/components/postWrapper.tsx
@@ -6,10 +6,12 @@ import post from "../../public/img/banner-top.jpg";
 import Image from "next/image";
 import HeartIcon from "./icons/heart";
 import FeedbackIcon from "./icons/feedback";
+import LineY from "./liney";
 
 const PostWrapper = () => {
   const [isActive, setIsActive] = useState(false);
   const [clicked, setClicked] = useState(false);
+  const [feedbackClick, setFeedbackClick] = useState(false);
   const active = () => {
     if (!isActive) {
       setIsActive(true);
@@ -22,40 +24,66 @@ const PostWrapper = () => {
   return (
     <Wrapper htmlElement="section" id="postwrapper">
       <div className="postwrapper__section">
-        <div className="top">
-          <div className="postwrapper__section-header">
-            <div className="header__picture">
-              <div className="picture__content">
-                <Image src={author} alt="author picture" placeholder="blur" />
+        <Wrapper>
+          <div className="top">
+            <div className="postwrapper__section-header">
+              <div className="header__picture">
+                <div className="picture__content">
+                  <Image src={author} alt="author picture" placeholder="blur" />
+                </div>
+              </div>
+              <div className="header__title">
+                <div className="name">Brian Tarqui</div>
+                <div className="date">July 2023</div>
               </div>
             </div>
-            <div className="header__title">
-              <div className="name">Brian Tarqui</div>
-              <div className="date">July 2023</div>
+            <div className="postwrapper__section-title">
+              <h4>Title Post</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+                nihil alias quas? Quidem, accusantium quae dolorem fuga iste
+                tempore corporis aliquam laudantium cum, minus voluptas
+                expedita, ut nostrum
+              </p>
             </div>
           </div>
-          <div className="postwrapper__section-title">
-            <h4>Title Post</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-              nihil alias quas? Quidem, accusantium quae dolorem fuga iste
-              tempore corporis aliquam laudantium cum, minus voluptas expedita,
-              ut nostrum
-            </p>
-          </div>
-        </div>
+        </Wrapper>
 
-        <div className="postwrapper__section-picture">
+        <Wrapper className="postwrapper__section-picture">
           <div className="picture__content" onDoubleClick={() => active()}>
             <Image src={post} alt="picture post" placeholder="blur" />
             <div className={`icon ${isActive ? "like" : ""}`}>
               <HeartIcon />
             </div>
           </div>
-        </div>
+        </Wrapper>
         <div className="postwrapper__section-options">
           <div className="options__content">
-            <button
+            <div className="box content__like">
+              <button
+                className={`heart__icon ${clicked ? "click" : ""}`}
+                onClick={() => {
+                  active();
+                  setClicked(true);
+                }}
+              >
+                <HeartIcon />
+              </button>
+            </div>
+            <LineY />
+            <div className="box content__comment">
+              <input
+                type="text"
+                name="comment"
+                id="comment"
+                placeholder="Give me your feedback"
+              />
+            </div>
+            <LineY />
+            <div className="box content__send">S</div>
+            <LineY />
+            <div className="box content__share">S</div>
+            {/* <button
               className={`heart__icon ${clicked ? "click" : ""}`}
               onClick={() => {
                 active();
@@ -71,8 +99,13 @@ const PostWrapper = () => {
                 id="desc"
                 placeholder="Give me your feedback"
               />
-              <FeedbackIcon />
-            </div>
+              <button
+                className={`feedback__icon ${feedbackClick ? "click" : ""}`}
+                onClick={() => setFeedbackClick(true)}
+              >
+                <FeedbackIcon />
+              </button>
+            </div> */}
           </div>
         </div>
       </div>
