19c434c16775f3ac7b4154937682bf87a0e798e5
brian tarqui <tarquibrian@gmail.com>
2023-08-10 12:19:57 -0400

    test

--- src/components/educationWrapper.tsx
+++ src/components/educationWrapper.tsx
@@ -5,33 +5,30 @@ import LineY from "./liney";
 const EducationWrapper = () => {
   return (
     <Wrapper id="education" htmlElement="section">
-      <div className="experiencewrapper__section">
+      <div className="educationwrapper__section">
         <Wrapper>
-          <div className="experiencewrapper__section-title">
+          <div className="educationwrapper__section-title">
             <h3>EDUCATION</h3>
           </div>
         </Wrapper>
-        <div className="experiencewrapper__section-list">
-          <Wrapper>
+        <div className="educationwrapper__section-list">
+          <div>
             <Wrapper className="list__header">
-              <div className="list__header-year">2021 - 2022</div>
+              <div className="list__header-year">2017 - 2022</div>
               <LineY />
               <div className="list__header-title">
-                <h4>Developer - Municipality of Villa Rivero</h4>
+                <h4>EMI - Military School of Engineering</h4>
               </div>
             </Wrapper>
             <div className="list__header-desc">
               <p>
-                Development of a web application focused on the management of
-                regional tourism, describing details such as: Analysis of the
-                problematic situation and development of a new alternative model
-                for the municipality. Use of geolocation for positioning of
-                tourist sites using Google Maps Platform tools. Integration of
-                progressive functionalities through PWA technology.Development,
-                deployment and testing of the system.
+                I earned a Bachelor's degree in Systems Engineering, equipping
+                me with versatile skills in technology, software development,
+                and complex problem-solving. This knowledge empowers me to
+                contribute effectively to the dynamic field of technology.
               </p>
             </div>
-          </Wrapper>
+          </div>
         </div>
       </div>
     </Wrapper>
