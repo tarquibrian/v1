eb525b38a0db2b2f52481d3b4b03a4af8f9dd7be
brian tarqui <tarquibrian@gmail.com>
2023-08-09 20:32:35 -0400

    test

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,19 +2,30 @@
 import Link from "next/link";
 import React from "react";
 import LineY from "./liney";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <nav>
-      <Link href={"/"}>
+      <Link
+        href={"/"}
+        className={pathname.split("/")[1] === "" ? "active" : ""}
+      >
         <span>POSTS</span>
       </Link>
       <LineY />
-      <Link href={"/projects"}>
+      <Link
+        href={"/projects"}
+        className={pathname.split("/")[1] === "projects" ? "active" : ""}
+      >
         <span>PROJECTS</span>
       </Link>
       <LineY />
-      <Link href={"/"}>
+      <Link
+        href={"/"}
+        className={pathname.split("/")[1] === "studio" ? "active" : ""}
+      >
         <span>STUDIO</span>
       </Link>
     </nav>
