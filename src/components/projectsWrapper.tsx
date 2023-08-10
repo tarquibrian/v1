b327c38ca95db11b965c58eb2d0e90d769fe6aae
brian tarqui <tarquibrian@gmail.com>
2023-08-09 21:39:53 -0400

    test

--- src/components/projectsWrapper.tsx
+++ src/components/projectsWrapper.tsx
@@ -41,6 +41,16 @@ const ProjectsWrapper = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="circle"
+            >
+              <circle cx="12" cy="12" r="12" />
+            </svg>
             <OpenIcon />
           </a>
         </div>
