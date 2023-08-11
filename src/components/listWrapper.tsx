ecf62e7cb8205efc26ed92a6620b320452bb4b4e
brian tarqui <tarquibrian@gmail.com>
2023-08-11 17:58:16 -0400

    test

--- src/components/listWrapper.tsx
+++ src/components/listWrapper.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Wrapper from "./wrapper";
 import Image from "next/image";
 import img from "../../public/img/banner-top.jpg";
+import LineY from "./liney";
 
 const ListWrapper = () => {
   return (
@@ -12,8 +13,13 @@ const ListWrapper = () => {
             <Image src={img} alt="picture project" placeholder="blur" />
           </div>
         </div>
+        <LineY />
         <div className="listwrapper__section-info">
-          
+          <h4>Title proejct</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae iure
+            placeat libero doloremque soluta eum iusto repellat repellendus
+          </p>
         </div>
       </div>
     </Wrapper>
