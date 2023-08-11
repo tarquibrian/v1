5c46e134e4565c7a3ef8b67ab6cc6c7418e33a49
brian tarqui <tarquibrian@gmail.com>
2023-08-11 15:20:58 -0400

    test

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,83 @@
+"use client";
 import ProjectsWrapper from "@/components/projectsWrapper";
-import React from "react";
+import React, { useState } from "react";
 import { ProjectsData } from "@/data/projectsData";
+import Wrapper from "@/components/wrapper";
+import LineY from "@/components/liney";
 
 const ProjectsPage = () => {
+  const [mode, setMode] = useState("mode1");
   return (
     <>
-      {ProjectsData.map((project) => {
+      <Wrapper id="project-nav">
+        <div className="nav">
+          <div className="nav-link">View my projects</div>
+          <LineY />
+          <div className="options">
+            <button
+              className="option__gallery"
+              onClick={() => setMode("mode1")}
+            >
+              <svg
+                width="46"
+                height="46"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M18 4H6a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Z"></path>
+                <path d="M18 14H6a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2Z"></path>
+              </svg>
+            </button>
+            <LineY />
+            <button
+              className="option__gallery"
+              onClick={() => setMode("mode2")}
+            >
+              <svg
+                width="46"
+                height="46"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M13 5h8"></path>
+                <path d="M13 9h5"></path>
+                <path d="M13 15h8"></path>
+                <path d="M13 19h5"></path>
+                <path d="M8 4H4a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1Z"></path>
+                <path d="M8 14H4a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1Z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </Wrapper>
+      {mode === "mode1" ? (
+        ProjectsData.map((project) => {
+          const { id, title, desc, picture, status, href } = project;
+          return (
+            <ProjectsWrapper
+              id={id}
+              title={title}
+              desc={desc}
+              picture={picture}
+              status={status}
+              href={href}
+            />
+          );
+        })
+      ) : (
+        <>hola</>
+      )}
+      {/* {ProjectsData.map((project) => {
         const { id, title, desc, picture, status, href } = project;
         return (
           <ProjectsWrapper
@@ -17,7 +89,7 @@ const ProjectsPage = () => {
             href={href}
           />
         );
-      })}
+      })} */}
     </>
   );
 };
--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -2,6 +2,7 @@ import { StaticImageData } from "next/image";
 import img from "../../public/img/banner-top.jpg";
 import project1picture from "../../public/img/project1-picture.png";
 import project2picture from "../../public/img/project2-picture.png";
+import project3picture from "../../public/img/project3-picture.png";
 
 interface Project {
   id: string;
@@ -48,11 +49,30 @@ export const ProjectsData: Project[] = [
     picture: project2picture,
     challenge:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nam, eaque, nobis ab saepe sit eum distinctio eius dolores rerum ducimus impedit illo in fugit consequuntur, iste voluptas neque placeat.",
-    solution: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nam, eaque, nobis ab saepe sit eum distinctio eius dolores rerum ducimus impedit illo in fugit consequuntur, iste voluptas neque placeat.",
+    solution:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nam, eaque, nobis ab saepe sit eum distinctio eius dolores rerum ducimus impedit illo in fugit consequuntur, iste voluptas neque placeat.",
     result:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nam, eaque, nobis ab saepe sit eum distinctio eius dolores rerum ducimus impedit illo in fugit consequuntur, iste voluptas neque placeat.",
     gallery: [img, img, img],
     status: true,
     href: "https://www.zonustech.com/",
+  },
+  {
+    id: "modernfashion",
+    title: "MODERFASHION",
+    desc: "Modern clothing ecommerce web application that offers a wide selection of clothing and fashion accessories for demanding customers.",
+    client: "Personal",
+    timeline: "3 Months",
+    services: ["Web Design", "Web Development", "UX/UI"],
+    picture: project3picture,
+    challenge:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nam, eaque, nobis ab saepe sit eum distinctio eius dolores rerum ducimus impedit illo in fugit consequuntur, iste voluptas neque placeat.",
+    solution:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nam, eaque, nobis ab saepe sit eum distinctio eius dolores rerum ducimus impedit illo in fugit consequuntur, iste voluptas neque placeat.",
+    result:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nam, eaque, nobis ab saepe sit eum distinctio eius dolores rerum ducimus impedit illo in fugit consequuntur, iste voluptas neque placeat.",
+    gallery: [img, img, img],
+    status: true,
+    href: "https://ecommerce-website-zeta-one.vercel.app/",
   },
 ];
