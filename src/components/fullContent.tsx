cb8572153ca6f5eeb428fe224417843a13313f9d
brian tarqui <tarquibrian@gmail.com>
2023-08-24 17:53:32 -0400

    test

--- src/components/fullContent.tsx
+++ src/components/fullContent.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import { ProjectsData } from "@/data/projectsData";
 import ProjectsWrapper from "./projectsWrapper";
 
-const getProjects = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/projects", {
-      cache: "no-store",
-    });
+// const getProjects = async () => {
+//   try {
+//     const res = await fetch("http://localhost:3000/api/projects", {
+//       cache: "no-store",
+//     });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch");
-    }
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch");
+//     }
 
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
+//     return res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-const FullContent = async () => {
+const FullContent = () => {
   // const { projects } = await getProjects();
   console.log("test");
   return (
