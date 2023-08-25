904222f77f4125c93454b02584cd84bcdea3e574
brian tarqui <tarquibrian@gmail.com>
2023-08-25 01:32:02 -0400

    test

--- src/app/projects/list/page.tsx
+++ src/app/projects/list/page.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import ListProjectsWrapper from "../listProjectsWrapper";
+import { ProjectsData } from "@/data/projectsData";
+import ListWrapper from "@/components/listWrapper";
 
 const List = () => {
-  return <ListProjectsWrapper />;
+  return ProjectsData.map((project) => {
+    const { id, title, desc, status, href, squarePicture, year, services } =
+      project;
+    return (
+      <ListWrapper
+        key={id}
+        id={id}
+        title={title}
+        squarePicture={squarePicture}
+        status={status}
+        href={href}
+        year={year}
+        desc={desc}
+        services={services}
+        // desc={desc}
+        // picture={picture}
+        // status={status}
+        // href={href}
+      />
+    );
+  });
 };
 
 export default List;
