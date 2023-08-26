c2075c24f4749ed3c4ae09d2112a6861339deb11
brian tarqui <tarquibrian@gmail.com>
2023-08-26 14:54:43 -0400

    test

--- src/components/NextImage.tsx
+++ src/components/NextImage.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { AdvancedImage, lazyload, placeholder } from "@cloudinary/react";
 import { cld } from "@/libs/cloudinary";
-import { Cloudinary } from "@cloudinary/url-gen";
 
 const NextImage = ({
   src,
@@ -17,18 +16,12 @@ const NextImage = ({
   width?: number;
   height?: number;
 }) => {
-  // const [isLoading, setIsLoading] = useState(true);
-  // const { CLOUDNAME } = process.env;
-  // const cld = new Cloudinary({
-  //   cloud: {
-  //     cloudName: "dskypy0xt",
-  //   },
-  // });
-  // console.log(process.env)
-
   const myImage = cld.image(src);
   return (
-    <AdvancedImage cldImg={myImage} plugins={[placeholder({ mode: "blur" })]} />
+    <AdvancedImage
+      cldImg={myImage}
+      plugins={[lazyload(), placeholder({ mode: "blur" })]}
+    />
   );
 };
 
--- src/libs/cloudinary.ts
+++ src/libs/cloudinary.ts
@@ -1,5 +1,4 @@
 import { Cloudinary } from "@cloudinary/url-gen";
-console.log(process.env.CLOUDNAME);
 
 export const cld = new Cloudinary({
   cloud: {
