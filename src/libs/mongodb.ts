69654be400ffe814ec20df01e1ad540c42d31e3d
brian tarqui <tarquibrian@gmail.com>
2023-08-31 00:46:46 -0400

    test

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const connectMongo = async () => {
   try {
     if (process.env.MONGODB_URI) {
-      await mongoose.connect(process.env.MONGODB_URI);
+      await mongoose.connect(
+        "mongodb+srv://root:dwRSOSYDeg1wQDRd@cluster0.qxrcb.mongodb.net/portfolio"
+      );
       console.log("DB CONNECTED");
     }
   } catch (error) {
