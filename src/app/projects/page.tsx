561a7c64e8e230bf495975fca3fb221a011daa92
brian tarqui <tarquibrian@gmail.com>
2023-08-30 23:59:44 -0400

    test

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,15 +2,41 @@ import ProjectsWrapper from "@/components/projectsWrapper";
 import React from "react";
 import { ProjectsData } from "@/data/projectsData";
 
+interface Project {
+  id: string;
+  title: string;
+  desc: string;
+  client: string;
+  timeline: string;
+  services: string[];
+  year?: number;
+  website?: string;
+  picture: Picture;
+  challenge: string;
+  solution: string;
+  result: string;
+  gallery: Picture[];
+  status: boolean;
+  href: string;
+  squarePicture: Picture;
+}
+
+interface Picture {
+  src: string;
+  alt: string;
+}
+
 const getProject = async () => {
-  const res = await fetch("http://localhost:3000/api/projects");
+  const res = await fetch("http://localhost:3000/api/projects", {
+    cache: "no-store",
+  });
   return res.json();
 };
 
 const ProjectsPage = async () => {
   const { projects } = await getProject();
   console.log("data", projects);
-  return ProjectsData.map((project) => {
+  return projects.map((project: Project) => {
     const { id, title, picture, status, href } = project;
     return (
       <ProjectsWrapper
