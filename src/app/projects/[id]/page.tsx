2363f3a2336f409b33de9f2f7cc7020e5a087fa7
brian tarqui <tarquibrian@gmail.com>
2023-09-03 23:36:11 -0400

    test

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ProjectsData } from "@/data/projectsData";
 
 const getProject = async (id: string) => {
-  const res = await fetch(`http://localhost:3000/api/projects/${id}`, {
+  const res = await fetch(`v1-hxd0un1t2-tarquibrian.vercel.app/api/projects/${id}`, {
     cache: "no-store",
   });
   return res.json();
