7a8b0c431057232b8170feafc08269f67153372d
brian tarqui <tarquibrian@gmail.com>
2023-09-05 23:42:56 -0400

    test

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 import Projects from "@/models/projects";
 
 export async function GET() {
-  await connectMongo();
-  const projects = await Projects.find();
-  return NextResponse.json({ projects });
+  try {
+    await connectMongo();
+    const projects = await Projects.find();
+    return NextResponse.json({ projects });
+  } catch (err) {
+    console.log(err);
+  }
 }
